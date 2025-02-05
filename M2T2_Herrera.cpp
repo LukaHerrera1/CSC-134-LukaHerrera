ee68e7aa1e31f3a5c29dc56522b258bcfdb5db09
LukaHerrera1 <herreral3581@student.faytechcc.edu>
2025-02-05 23:49:19 +0000

    M2T2 Save 3

--- M2T2_Herrera.cpp
+++ M2T2_Herrera.cpp
@@ -9,12 +9,10 @@ using namespace std;
 
 int main()
 {
-    string name;
-    cout << "Please enter your name: ";
-    cin >> name; 
-    string
-    int num_apples;
-    double cost_each;
+
+    string name = "Luka";
+    int num_apples = 100;
+    double cost_each = 0.25;
 
     string user_name; 
     int apples_to_buy;
@@ -24,20 +22,20 @@ int main()
     cout << "Thanks for coming by, " << user_name << "!" << endl;
 
     cout << "Welcome to the " << name << "'s apple farm!" << endl;
-    cout << "There are " << num_apples << " in stock" << endl; 
-    cout << "They cost $" << cost_each << endl;
+    cout << "There are " << num_apples << " apples in stock" << endl; 
+    cout << "They each cost $" << cost_each << endl;
 
     cout << setprecision(2) << fixed;
 
     double total_price = num_apples * cost_each;
      
-    cout << "The price for all of them is " << total_price << endl;
+    cout << "The price for all of them is $" << total_price << endl;
     
     cout << "How many apples would you like? ";
     cin >> apples_to_buy;
 
     total_price = apples_to_buy * cost_each;
-
+    // 
     cout << "Thank you! " << apples_to_buy << " apples comes to $" << total_price << endl;
     cout << "Have a nice day!" << endl;
     cout << endl; 
