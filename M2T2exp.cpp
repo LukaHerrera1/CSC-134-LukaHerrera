0437696e49eaf5487719e4cd42992f00996413ca
LukaHerrera1 <herreral3581@student.faytechcc.edu>
2025-02-07 19:26:38 +0000

    Save 5

--- M2T2exp.cpp
+++ M2T2exp.cpp
@@ -4,34 +4,35 @@
 // 2/5/2025
 
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
-int main ()
-{ 
+int main()
+{
     string name;
     int num_apples;
-    double price_per_apple;
+    double price_per_apple
 
-    // User input of apple farm information
+
     cout << "What is your name?" << endl;
     cin >> name;
+    
     cout << "How many apples are for sale?" << endl;
-    cin >> num_apples;
-    cout << setprecision(2) << fixed;
+    cin num_apples;
     cout << "How much are each apple?" << endl;
     cin >> price_per_apple;
-
-    // Greeting message for the apple farm
-    cout << "Welcome to " << name << "'s apple farm!" << endl;
-    cout << "There are " << num_apples << " apples in stock" << endl;
+    
+    cout << "Welcome to the " << name << "'s apple farm!" << endl;
+    cout << "There are " << num_apples << " apples in stock" << endl; 
     cout << "They each cost $" << price_per_apple << endl;
 
     cout << setprecision(2) << fixed;
 
     double total_price = num_apples * price_per_apple;
-    
+     
     cout << "The price for all of them is $" << total_price << endl;
-
+    
+    cout << "Thank you! " << apples_to_buy << " apples comes to $" << total_price << endl;
+    cout << "Have a nice day!" << endl;
+    cout << endl; 
     return 0;
 }
