d6d2d2a57226eb9129155be5a839ca38b71e1343
LukaHerrera1 <herreral3581@student.faytechcc.edu>
2025-02-14 23:58:46 +0000

    M1Lab1 Save 3

--- M1Lab1_Herrera.cpp
+++ M1Lab1_Herrera.cpp
@@ -17,24 +17,27 @@ int main() {
     const double cost_per_cubic_foot = 0.23;
     const double charge_per_cubic_foot = 0.50;
 
-    cout << "Enter the dimensions of the crate below (in feet)." << endl;
+    cout << "Please enter the dimensions of the crate below (in feet)." << endl;
 
-    cout << "Width: ";
-    cin >> width;
+    // User imput for dimensions of the crate
     cout << "Length: ";
     cin >> length;
+    cout << "Width: ";
+    cin >> width;
     cout << "Height: ";
     cin >> height;
 
+    // Math used to find the volume and cost of the crate
     volume = width * length * height;
     cost = volume * cost_per_cubic_foot;
     charge = volume * charge_per_cubic_foot;
     profit = charge - cost;
 
+    // Results of input and math
     cout << setprecision(2) << fixed << showpoint; 
     cout << "The volume of the crate is " << volume << " cubic feet." << endl;
-    cout << "The cost to make a " << volume << " cubic foot crate would be: $" << cost << "." << endl;
-    cout << "The price it will be sold will be: $" << charge << "." << endl;
+    cout << "The cost to make a " << volume << " cubic foot crate is: $" << cost << "." << endl;
+    cout << "The price it will be sold at is: $" << charge << "." << endl;
     cout << "The profit of the create is: $" << profit << "." << endl;
 
     return 0;
