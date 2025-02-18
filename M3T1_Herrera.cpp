cc6fe608b0b5c56ac70fbe2a37a3579d91f837ac
LukaHerrera1 <herreral3581@student.faytechcc.edu>
2025-02-18 14:56:51 +0000

    M3T1 Save 1

---
+++ M3T1_Herrera.cpp
@@ -0,0 +1,16 @@
+// CSC 134
+// M3T1
+// Luka Herrera
+// 2/18/2025
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    int width_one, length_one, width_two, lenght_two;
+    int area_one, area_two;
+
+    
+}
