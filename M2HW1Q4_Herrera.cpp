a50520c609a3c047744df1998e79ccb37a611a4e
LukaHerrera1 <herreral3581@student.faytechcc.edu>
2025-02-18 14:46:30 +0000

    M2HW1 Q4 Save 1

---
+++ M2HW1Q4_Herrera.cpp
@@ -0,0 +1,26 @@
+// CSC 134
+// M2HW1 - Gold (Question 4)
+// Luka Herrera
+// 2/18/2025
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string school = " FTCC";
+    string team_name = " Trojans";
+    string lets_go = "Let's go";
+    string chear_one, chear_two;
+
+    chear_one = lets_go + school;
+    chear_two = lets_go + team_name;
+
+    cout << chear_one << endl;
+    cout << chear_one << endl;
+    cout << chear_one << endl;
+    cout << chear_two << endl;
+
+    return 0;
+}
