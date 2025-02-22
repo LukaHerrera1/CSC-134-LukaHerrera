e37c7f796b408a53b11816b4eedb2a2ba3faa0f9
LukaHerrera1 <herreral3581@student.faytechcc.edu>
2025-02-22 23:59:13 +0000

    M2HW1 Q3 Save 2

--- M2HWQ3_Herrera.cpp
+++ M2HWQ3_Herrera.cpp
@@ -11,8 +11,9 @@ int main () {
 
     int vistors, pizzas_order, slice_per_pizza;
 
+    // user input
     cout << "Pizza Party Caculator" << endl;
-    cout << "Caculates for 3 slices of pizza per person" << endl;
+    cout << "Caculates for 3 slices of pizza per person." << endl;
     cout << "To get started enter the number of vistors: ";
     cin >> vistors;
     cout << "How many pizza's did you order: ";
@@ -20,6 +21,7 @@ int main () {
     cout << "How many slices per pizza: ";
     cin >> slice_per_pizza;
 
+    // caculations
     int pizzas_left_over = (pizzas_order * slice_per_pizza)-(vistors * 3);
     cout << "There are " << pizzas_left_over << " slices of pizza left over." << endl;
 
