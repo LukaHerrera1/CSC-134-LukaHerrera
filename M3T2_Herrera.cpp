cc03c07d5b6ce3940231d87aedb34dbb6d84f0c6
LukaHerrera1 <herreral3581@student.faytechcc.edu>
2025-02-22 23:22:47 +0000

    M3T2 Save 2

--- M3T2_Herrera.cpp
+++ M3T2_Herrera.cpp
@@ -8,9 +8,11 @@ using namespace std;
 
 int main() {
 
+    // declare variables
     int width_one, length_one, width_two, length_two;
     int area_one, area_two;
 
+    // ask for user input
     cout << "Please enter the width and length of the first rectangle, seperated by";
     cout << " a space or new line." << endl;
     cin >> width_one >> length_one;
@@ -19,12 +21,15 @@ int main() {
     cout << " a space or new line." << endl;
     cin >> width_two >> length_two;
 
+    // caculate results
     area_one = width_one * length_one;
     area_two = width_two * length_two;
 
+    // display results
     cout << "Rectangle one has an area of " << area_one << "." << endl;
     cout << "Rectangle two has an area of " << area_two << "." << endl;
 
+    // determines which is bigger
     if (area_one > area_two) {
         cout << "Rectangle one is bigger." << endl;
 
