287798f8a3a2f1b1516dc6409b0a6cd9818fb421
LukaHerrera1 <herreral3581@student.faytechcc.edu>
2025-03-13 13:49:05 +0000

    M4T1 Save 1 (Both Examples)

---
+++ M4T1(Example 1)_Herrera.cpp
@@ -0,0 +1,22 @@
+// CSC - 134
+// M4T1 - Example One
+// Luka Herrera
+// 3/13/2025
+
+#include <iostream>
+using namespace std;
+
+// Example One
+
+int main() {
+
+    int number = 0;
+
+    while (number < 5)
+    {
+        cout << "Hello\n";
+        number ++;
+    }
+    cout << "That's all!\n";
+    return 0;
+}
