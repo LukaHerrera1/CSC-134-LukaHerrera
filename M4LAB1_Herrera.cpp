6fd5808a4e85a93516c8a8afae69cff89836e7c8
LukaHerrera1 <herreral3581@student.faytechcc.edu>
2025-03-26 13:51:20 +0000

    M4LAB1 Save 2

--- M4LAB1_Herrera.cpp
+++ M4LAB1_Herrera.cpp
@@ -16,15 +16,18 @@ int main() {
     cout << "Enter height: ";
     cin >> height;
 
+    cout << "One Row" << endl;
     for (int i=0; i < width; i++) {
         cout << "*" << " ";
     }
     cout << endl;
 
+    cout << "One Column" << endl;
 for (int j=0; j < height; j++) { 
     cout << "*" << endl;
 }
 
+cout << "The Entire Block" << endl;
 for (int j=0; j < height; j++) {
     
     for (int i=0; i < width; i++){
