e2d8be2f004608955385420353fce265d198dc5a
LukaHerrera1 <herreral3581@student.faytechcc.edu>
2025-04-18 17:21:55 +0000

    Save 1 for Q1M5HW

---
+++ M5HW1Q1_Herrera.cpp
@@ -0,0 +1,29 @@
+// CSC 134
+// M5HW1 - Question 1
+// Luka Herrera
+// 4/18/2025
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    string first_month, second_month, third_month;
+    double first_month_rainfall, second_month_rainfall, third_month_rainfall;
+
+    cout << "Enter month: ";
+    cin >> first_month;
+    cout << "Enter rainfall for
+    cin >> first_month_rainfall;
+    cout << "Enter month: ";
+    cin >> second_month;
+    cout << "Enter rainfall: ";
+    cin >> second_month_rainfall;
+    cout << "Enter month: ";
+    cin >> third_month;
+    cout << "Enter rainfall: ";
+    cin >> third_month_rainfall;
+
+
+    return 0;
+}
