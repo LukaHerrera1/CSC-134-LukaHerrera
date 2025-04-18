be6b83c6ba1130e89cac1e5c65d9c8ff0a3fd942
LukaHerrera1 <herreral3581@student.faytechcc.edu>
2025-04-18 17:36:34 +0000

    Q1 for M5HW save 2

--- M5HW1Q1_Herrera.cpp
+++ M5HW1Q1_Herrera.cpp
@@ -4,6 +4,7 @@
 // 4/18/2025
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
@@ -13,17 +14,22 @@ int main() {
 
     cout << "Enter month: ";
     cin >> first_month;
-    cout << "Enter rainfall for
+    cout << "Enter rainfall for " << first_month << ": ";
     cin >> first_month_rainfall;
     cout << "Enter month: ";
     cin >> second_month;
-    cout << "Enter rainfall: ";
+    cout << "Enter rainfall for " << second_month << ": ";
     cin >> second_month_rainfall;
     cout << "Enter month: ";
     cin >> third_month;
-    cout << "Enter rainfall: ";
+    cout << "Enter rainfall for " << third_month << ": ";
     cin >> third_month_rainfall;
 
+    cout << fixed << setprecision(2);
+    double average_rainfall = (first_month_rainfall + second_month_rainfall + third_month_rainfall) / 3.0;
+
+    cout << "The average rainfall for " << first_month << ", " << second_month << ", and " << third_month;
+    cout << " is " << average_rainfall << " inches." << endl;
 
     return 0;
 }
